8d3d577b3998f944a01dc775c0b12f1452e2a776
Jayne Hsu <jaynehsu@ebay.com>
2018-12-29 13:15:32 -0800

    modified

--- src/main/java/tips/Sandbox2.java
+++ src/main/java/tips/Sandbox2.java
@@ -1,4 +1,13 @@
 package tips;
 
 public class Sandbox2 {
+    public static void main(String[] args) {
+        String str = "1234567";
+
+
+        int i = 0;
+        System.out.println(str.substring(0,7));
+        System.out.println(str.substring(0,1));
+
+    }
 }
